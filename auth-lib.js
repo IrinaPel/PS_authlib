eb69ebfc3217574581d09ccc75406803f6b0992c
IrinaPel <ira_pel72@mail.ru>
2019-03-14 22:05:45 +0500

    step 1

--- auth-lib.js
+++ auth-lib.js
@@ -1,8 +1,37 @@
-function createUser() {};
+var allUsers = [
+	{nickname: "admin", password: "1234", groups: ["admin", "manager", "basic"]},
+	{nickname: "sobakajozhec", password: "ekh228", groups: ["basic", "manager"]},
+	{nickname: "patriot007", password: "russiaFTW", groups: ["basic"]}
+];
+var allRights = ["manage content", "play games", "delete users", "view site"];
+var allGroups = {
+	"admin": [rights[2]],
+	"manager": [rights[0]],
+	"basic": [rights[1], rights[3]]
+}
 
-function deleteUser() {};
+//Создает нового пользователя с указанным логином username и паролем password, возвращает созданного пользователя. 
+function createUser(username, pass) {
+	let newUser = {nickname: username, password: pass, groups: []};
+	allUsers.push(newUser);
+	return newUser;
+};
 
-function users() {};
+//Удаляет пользователя user 
+function deleteUser(user) {
+	let index = allUsers.indexOf(user);
+	if (index > -1) {
+		allUsers.splice(index, 1);
+	} else {
+		throw new Error("error");
+	}
+
+};
+
+//Возвращает массив всех пользователей. 
+function users() {
+	return allUsers; 
+};
 
 function createGroup() {};
 
@@ -12,7 +41,11 @@ function groups() {};
 
 function addUserToGroup() {};
 
-function userGroups() {};
+//Возвращает массив групп, к которым принадлежит пользователь user 
+function userGroups(user) {
+	let index = allUsers.indexOf(user);
+	return allUsers[index]["groups"];
+};
 
 function removeUserFromGroup() {};
 
