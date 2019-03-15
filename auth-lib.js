e92fc9327f1bb5824310a9b35e14358cf5abee9a
IrinaPel <ira_pel72@mail.ru>
2019-03-15 18:54:10 +0500

    step 2 done

--- auth-lib.js
+++ auth-lib.js
@@ -5,9 +5,9 @@ var allUsers = [
 ];
 var allRights = ["manage content", "play games", "delete users", "view site"];
 var allGroups = {
-	"admin": [rights[2]],
-	"manager": [rights[0]],
-	"basic": [rights[1], rights[3]]
+	"admin": [allRights[2]],
+	"manager": [allRights[0]],
+	"basic": [allRights[1], allRights[3]]
 }
 
 //Создает нового пользователя с указанным логином username и паролем password, возвращает созданного пользователя. 
@@ -23,7 +23,7 @@ function deleteUser(user) {
 	if (index > -1) {
 		allUsers.splice(index, 1);
 	} else {
-		throw new Error("error");
+		throw new Error("error deleteUser");
 	}
 
 };
@@ -33,13 +33,49 @@ function users() {
 	return allUsers; 
 };
 
-function createGroup() {};
+//Создает новую группу и возвращает её
+function createGroup() {
+	let newGroup = guidGenerator();
+	allGroups[newGroup] = [];
+	return newGroup;
+};
 
-function deleteGroup() {};
+//Удаляет группу group 
+function deleteGroup(group) {
+	if (group && allGroups[group]){
+		for (let i = 0 ; i < allUsers.length ; i++){
+			//Ловим исключение из функции на отсутствие группы у пользователя
+			try {
+				removeUserFromGroup(allUsers[i], group);
+			} catch {
+			}
+		}
+		delete allGroups[group];
+	} else {
+		throw new Error('opa');
+	}
+};
 
-function groups() {};
+//Возвращает массив групп 
+function groups() {
+	return Object.keys(allGroups);
+};
 
-function addUserToGroup() {};
+//Добавляет пользователя user в группу group 
+function addUserToGroup(user, group) {
+	let index = allUsers.indexOf(user);	
+	if (index > -1 && user && group) { 
+		let indexGroup = allUsers[index]["groups"].indexOf(group);
+		if (indexGroup === -1) {
+			allUsers[index]["groups"].push(group);
+		} else {
+			//console.log(indexGroup, group, user, allUsers);
+			//throw new Error("error: group existing");
+		}
+	} else {
+		throw new Error("error addUserToGroup");
+	} 
+};
 
 //Возвращает массив групп, к которым принадлежит пользователь user 
 function userGroups(user) {
@@ -47,24 +83,153 @@ function userGroups(user) {
 	return allUsers[index]["groups"];
 };
 
-function removeUserFromGroup() {};
+//Удаляет пользователя user из группы group. Должна бросить исключение, если пользователя user нет в группе group 
+function removeUserFromGroup(user, group) {
+	let index = allUsers.indexOf(user);	 
+	if (index > -1) { 
+		let indexGroup = allUsers[index]["groups"].indexOf(group);
+		if (indexGroup > -1) {
+			allUsers[index]["groups"].splice(indexGroup, 1);;
+		} else {
+			throw new Error("error: group not existing");
+		}
+	} else {
+		throw new Error("error removeUserFromGroup");
+	} 
+};
 
-function createRight() {};
+function guidGenerator() {
+    var S4 = function() {
+       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    };
+    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+}
 
-function deleteRight() {};
+//Создает новое право и возвращает его 
+function createRight() {
+	let newRight = guidGenerator();
+	allRights.push(newRight);
+	return newRight;
+};
 
-function groupRights() {};
+//Удаляет право right и каскадно удаляет связь с группой
+function deleteRight(right) {
+	let index = allRights.indexOf(right);
+	if (index > -1) {
+		for (let i = 0 ; i < Object.keys(allGroups).length ; i++){
+			//Ловим исключение из функции на отсутствие группы у пользователя
+			try {
+				removeRightFromGroup(right, Object.keys(allGroups)[i]);
+			} catch {
+			}
+		}
+		allRights.splice(index, 1);
+	} else {
+		throw new Error("error deleteRight");
+	}
+};
 
-function rights() {};
+//Возвращает массив прав, которые принадлежат группе group
+function groupRights(group) {
+	return allGroups[group];
+};
 
-function addRightToGroup() {};
+//Возвращает массив прав
+function rights() {
+	return allRights; 
+};
 
-function removeRightFromGroup() {};
+//Добавляет право right к группе group
+function addRightToGroup(right, group) {
+	let index = allRights.indexOf(right);	
+	// let checkGroup = allGroups[group]; 
+	if (index > -1) { 
+		let searchedGroup = allGroups[group];
+		if (searchedGroup) {
+			allGroups[group].push(right);
+		} else {
+			throw new Error("error: group not existing");
+		}
+	} else {
+		throw new Error("error addRightToGroup");
+	} 
+};
 
-function login(username, password) {};
+//Удаляет право right из группы group. Должна бросить исключение, если права right нет в группе group 	
+function removeRightFromGroup(right, group) {
+	let index = allRights.indexOf(right);	
+	// let checkGroup = allGroups[group]; 
+	if (index > -1) { 
+		let indexRight = allGroups[group].indexOf(right);
+		if (indexRight > -1) {
+			allGroups[group].splice(indexRight, 1);;
+		} else {
+			throw new Error("error: right not existing");
+		}
+	} else {
+		throw new Error("error removeRightFromGroup: You are so fast 0_o RETRY NOW!!!!!");
+	} 
+};
 
-function currentUser() {};
+//Сессия пользователя
+var currentSessionUser = undefined;
 
-function logout() {};
+//return -	true, если пользователь с логином username и паролем password существует, false в противном случае. 
+//Также функция login должна вернуть false в случае, если сессия пользователя уже существует.
+function login(username, password) {
+	let userFinded = false;
+	for (let i = 0; i < allUsers.length; i++){
+		if (allUsers[i]['nickname'] == username && allUsers[i]['password'] == password){
+			userFinded = allUsers[i];
+			break;
+		}
+	}
 
-function isAuthorized(user, right) {};
+	if (userFinded && !currentSessionUser){
+		currentSessionUser = userFinded;
+		return true;
+	} else {
+		return false;
+	}
+};
+
+//return	-	Возвращает текущего пользователя или undefined в случае, если текущий пользователь не прошел аутентифицацию
+function currentUser() {
+	return currentSessionUser;
+};
+
+// Вызов данной функции должен завершать сессию текущего пользователя
+function logout() {
+	currentSessionUser = undefined;
+};
+
+//return - true в случае, если пользователь user обладает правом right, false в противном случае
+function isAuthorized(user, right) {
+	if (!user || !right){
+		throw new Error('user or right not valid');
+	}
+
+
+	let userIndex = allUsers.indexOf(user);
+	let rightIndex = allRights.indexOf(right);
+	if (userIndex > -1 && rightIndex > -1){
+		let curUserGroups = userGroups(user);
+		for(let i = 0; i < curUserGroups.length; i++){
+			if (allGroups[curUserGroups[i]].indexOf(right) > -1){
+				return true;
+			}
+		}
+		return false;
+	} else {
+		throw new Error('user or right not existing');
+	}
+};
+
+
+
+
+
+
+
+
+
